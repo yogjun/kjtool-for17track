b0390e12b8496bbe1afd6de6fb93bb5826c84a67
yogjun <280536928@qq.com>
2024-09-26 18:21:50 +0800

    添加默认路由

--- src/main/java/com/yogjun/kjtoolfor17track/DefaultController.java
+++ src/main/java/com/yogjun/kjtoolfor17track/DefaultController.java
@@ -6,18 +6,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * {@link TestController}
+ * {@link DefaultController}
  *
  * @author <a href="mailto:280536928@qq.com">yogjun</a>
  * @version ${project.version} - 2024/9/20
  */
-@RequestMapping("/test")
+@RequestMapping("")
 @Controller
-public class TestController {
+public class DefaultController {
 
   @RequestMapping("")
   public String index(Model model) {
-    model.addAttribute("location", new Location("北京", "中国的首都", 39.9042, 116.4074));
-    return "test.html";
+    return "index.html";
   }
 }
